5ddc8b64ad2a773b4a8acf267fff1b93805302e6
nathan-nigussie <91279474+nathan-nigussie@users.noreply.github.com>
2024-05-30 18:41:58 +0300

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
-import javax.swing.JOptionPane;
 
+import java.util.Random;
 
+//generating random numbers from negative 2 billion to plus 2 billion
 public class Main {
     public static void main(String[] args) {
-       String name=JOptionPane.showInputDialog("Enter Your Name");
-       JOptionPane.showMessageDialog(null,"hello" + name);
-       int age=Integer.parseInt(JOptionPane.showInputDialog("Enter Your age"));
-       JOptionPane.showMessageDialog(null,"You are " + age + "years old");
 
-       //if we want to parse a double value
-        double height=Double.parseDouble(JOptionPane.showInputDialog("Enter Your Height"));
-        JOptionPane.showMessageDialog(null,"You are " + height + "CM");
-
-
-
-
+        Random random = new Random();
+        int x = random.nextInt();
+        System.out.println(x);
 
     }
 }
