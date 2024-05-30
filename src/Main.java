aeea25ae22fa58fd0022f7315450c85ef36d72f1
nathan-nigussie <91279474+nathan-nigussie@users.noreply.github.com>
2024-05-30 18:10:11 +0300

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.JOptionPane;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String name=JOptionPane.showInputDialog("Enter Your Name");
+       JOptionPane.showMessageDialog(null,"hello" + name);
+       int age=Integer.parseInt(JOptionPane.showInputDialog("Enter Your age"));
+       JOptionPane.showMessageDialog(null,"You are " + age + "years old");
+
+       //if we want to parse a double value
+        double height=Double.parseDouble(JOptionPane.showInputDialog("Enter Your Height"));
+        JOptionPane.showMessageDialog(null,"You are " + height + "CM");
+
+
+
+
+
     }
 }
